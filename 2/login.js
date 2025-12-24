82a20c53b93523ed328ce6725c17cf6ee9e15f17
Muslich <muslichwahyuromadhon@gmail.com>
2025-12-24 16:37:15 +0700

    Menambahkan fitur login sederhana

--- 2/login.js
+++ 2/login.js
@@ -1,3 +1,12 @@
-function login() {
-    console.log("Login");
+function login(username, password) {
+    if (username === "admin" && password === "12345") {
+        console.log("Login berhasil! Selamat datang, Admin.");
+        return true;
+    } else {
+        console.log("Login gagal! Username atau password salah.");
+        return false;
+    }
 }
+
+// Test function
+login("admin", "12345");
