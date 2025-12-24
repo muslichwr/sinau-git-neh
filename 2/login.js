361671849be43f82e8a834e0698246e7059bfcdb
Muslich <muslichwahyuromadhon@gmail.com>
2025-12-24 16:47:05 +0700

    Mengubah pesan login di fitur-login

--- 2/login.js
+++ 2/login.js
@@ -1,6 +1,6 @@
 function login(username, password) {
     if (username === "admin" && password === "12345") {
-        console.log("Login berhasil! Selamat datang, Admin.");
+        console.log("🎉 Berhasil Login! Halo, Admin!");  // ← UBAH BARIS INI (BERBEDA!)
         return true;
     } else {
         console.log("Login gagal! Username atau password salah.");
